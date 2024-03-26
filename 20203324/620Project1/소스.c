42b4d7bbc9636876535a9259b2c84c8acec54ee3
jhyun1234 <anjyounl24@gmail.com>
2024-03-26 11:47:59 +0900

    2024.3.26

--- 20203324/620Project1/소스.c
+++ 20203324/620Project1/소스.c
@@ -1,9 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+//fabs(a - b) < 0.0001 실수가 포함되었을때 비교
 int main()
 {
-
+	unsigned char a = 1;
+	char b = -1;
+	printf("a<<1 = %X\n", a << 1);
+	printf("b>>1 = %X\n", b >> 1);
 
 
 	return 0;
