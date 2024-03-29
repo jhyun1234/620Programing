334708a0829de48dda3bbfdf2fef90beae9e8ba9
jhyun1234 <anjyounl24@gmail.com>
2024-03-29 13:20:19 +0900

    2024.3.29 C언어 공부

--- 20203324/620Project1/소스.c
+++ 20203324/620Project1/소스.c
@@ -1,14 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
 //fabs(a - b) < 0.0001 실수가 포함되었을때 비교
+
+
+
+int AddToTotal(int num)
+{
+	static int total = 0;
+	total += num;
+	return total;
+}
+
+
+
+
 int main()
 {
-	unsigned char a = 1;
-	char b = -1;
-	printf("a<<1 = %X\n", a << 1);
-	printf("b>>1 = %X\n", b >> 1);
-
-
+	int num,i;
+	for (i = 0; i < 3; i++)
+	{
+		printf("입력 %d : ", i + 1);
+		scanf("%d", &num);
+		printf("누적 : %d \n", AddToTotal(num));
+	}
 	return 0;
 }
+
